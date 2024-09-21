d266e7317c3bc9a245d243e137a3fdf19666509f
Mickle <misha_2003_5@mail.ru>
2024-09-21 22:15:27 +0500

    micronew lks

--- src/pages/Company.js
+++ src/pages/Company.js
@@ -16,7 +16,7 @@ const Personal = () => {
                     <div className="categories">
                         <h2 className="title-2">Выберите категорию</h2>
                         <ul>
-                            <li onClick={() => handleCategoryChange('personalInfo')}>Личная информация</li>
+                            <li onClick={() => handleCategoryChange('personalInfo')}>Информация о компании</li>
                             <li onClick={() => handleCategoryChange('interests')}>Мои интересы</li>
                         </ul>
                     </div>
@@ -24,20 +24,16 @@ const Personal = () => {
                     <div className="content">
                         {selectedCategory === 'personalInfo' && (
                             <div className="personal-info">
-                                <h2 className="title-2">Личная информация</h2>
-                                <input type="text" placeholder="Имя" />
-                                <input type="text" placeholder="Фамилия" />
-                                <input type="text" placeholder="Отчество" />
-                                <input type="number" placeholder="Возраст" />
-                                <p type="text">Пол</p>
-                                <select>
-                                    <option value=""></option>
-                                    <option value="male">Мужской</option>
-                                    <option value="female">Женский</option>
-                                </select>
-                                <input type="text" placeholder="Студенческий билет" />
+                                <h2 className="title-2">Информация о компании</h2>
+                                <input type="text" placeholder="Название" />
                                 <input type="text" placeholder="Email" />
-                                <input type="number" placeholder="Номер телефона" />
+                                <input type="number" placeholder="Регистрационный номер" />
+                                <input type="text" placeholder="ФИО Контактного лица" />
+                                <input type="text" placeholder="Номер телефона" />
+                                <input type="text" placeholder="Адрес" />
+                                <input type="text" placeholder="Сайт компании" />
+                                
+                                
 
                                 <button>Сохранить изменения</button>
                             </div>
@@ -49,84 +45,8 @@ const Personal = () => {
                                 <label>
     <input type="checkbox" value="Гостиницы, рестораны, общепит" /> Гостиницы, рестораны, общепит
 </label>
-<label>
-    <input type="checkbox" value="Государственные организации" /> Государственные организации
-</label>
-<label>
-    <input type="checkbox" value="Добывающая отрасль" /> Добывающая отрасль
-</label>
-<label>
-    <input type="checkbox" value="Информационные технологии, системная интеграция, интернет" /> Информационные технологии, системная интеграция, интернет
-</label>
-<label>
-    <input type="checkbox" value="Искусство, культура" /> Искусство, культура
-</label>
-<label>
-    <input type="checkbox" value="Лесная промышленность, деревообработка" /> Лесная промышленность, деревообработка
-</label>
-<label>
-    <input type="checkbox" value="Медицина, фармацевтика, аптеки" /> Медицина, фармацевтика, аптеки
-</label>
-<label>
-    <input type="checkbox" value="Металлургия, металлообработка" /> Металлургия, металлообработка
-</label>
-<label>
-    <input type="checkbox" value="Нефть и газ" /> Нефть и газ
-</label>
-<label>
-    <input type="checkbox" value="Образовательные учреждения" /> Образовательные учреждения
-</label>
-<label>
-    <input type="checkbox" value="Общественная деятельность" /> Общественная деятельность
-</label>
-<label>
-    <input type="checkbox" value="Перевозки, логистика, склад" /> Перевозки, логистика, склад
-</label>
-<label>
-    <input type="checkbox" value="Продукты питания" /> Продукты питания
-</label>
-<label>
-    <input type="checkbox" value="Промышленное оборудование, техника, станки и комплектующие" /> Промышленное оборудование, техника, станки и комплектующие
-</label>
-<label>
-    <input type="checkbox" value="Розничная торговля" /> Розничная торговля
-</label>
-<label>
-    <input type="checkbox" value="СМИ, маркетинг, реклама, BTL, PR, дизайн, продюсирование" /> СМИ, маркетинг, реклама, BTL, PR, дизайн, продюсирование
-</label>
-<label>
-    <input type="checkbox" value="Сельское хозяйство" /> Сельское хозяйство
-</label>
-<label>
-    <input type="checkbox" value="Строительство, недвижимость, эксплуатация, проектирование" /> Строительство, недвижимость, эксплуатация, проектирование
-</label>
-<label>
-    <input type="checkbox" value="Телекоммуникации, связь" /> Телекоммуникации, связь
-</label>
-<label>
-    <input type="checkbox" value="Тяжелое машиностроение" /> Тяжелое машиностроение
-</label>
-<label>
-    <input type="checkbox" value="Управление многопрофильными активами" /> Управление многопрофильными активами
-</label>
-<label>
-    <input type="checkbox" value="Услуги для бизнеса" /> Услуги для бизнеса
-</label>
-<label>
-    <input type="checkbox" value="Услуги для населения" /> Услуги для населения
-</label>
-<label>
-    <input type="checkbox" value="Финансовый сектор" /> Финансовый сектор
-</label>
-<label>
-    <input type="checkbox" value="Химическое производство, удобрения" /> Химическое производство, удобрения
-</label>
-<label>
-    <input type="checkbox" value="Электроника, приборостроение, бытовая техника, компьютеры и оргтехника" /> Электроника, приборостроение, бытовая техника, компьютеры и оргтехника
-</label>
-<label>
-    <input type="checkbox" value="Энергетика" /> Энергетика
-</label>
+
+
                                 <button>Сохранить изменения</button>
                             </div>
                         )}
