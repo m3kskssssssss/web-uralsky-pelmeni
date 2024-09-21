1bdaf09aaaf571f038301fc4c1a0fc6a4ac5a38f
mruax <matvey.chernyakov@yandex.ru>
2024-09-21 23:27:22 +0500

    Карточки покрасивее

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,6 +1,7 @@
 import BtnGitHub from "../components/btnGitHub/BtnGitHub";
 import { useParams } from "react-router-dom";
 import { projects } from "./../helpers/projectList";
+import './Project.css'; // Добавьте импорт стилей
 
 const Project = () => {
     const { id } = useParams(); // Получаем id из параметров маршрута
@@ -11,71 +12,56 @@ const Project = () => {
             <div className="container">
                 {project && (
                     <div className="project-details">
-
                         {/* Заголовок проекта */}
                         <h2 className="title-1">{project.specialty}</h2>
 
-                        {/* Изображение проекта */}
-                        <img
-                            src={project.imgBig}
-                            alt={project.title}
-                            className="project-details__cover"
-                            style={{ marginBottom: "20px" }} /* Отступ снизу от изображения */
-                        />
+                        <div className="project-details__content">
+                            {/* Изображение проекта */}
+                            <img
+                                src={project.imgBig}
+                                alt={project.title}
+                                className="project-details__cover"
+                            />
 
-                        {/* Описание проекта */}
-                        <div className="project-details__desc">
+                            {/* Описание проекта */}
+                            <div className="project-details__desc">
+                                {/* Даты проведения */}
+                                <p><strong>Даты проведения: </strong> {project.date}</p>
 
-                            {/* Даты проведения */}
-                            <p>
-                                <strong>Даты проведения: </strong> {project.date}
-                            </p>
+                                {/* Описание практики */}
+                                <p><strong>О практике: </strong> {project.description}</p>
 
-                            {/* Описание практики */}
-                            <p>
-                                <strong>О практике: </strong> {project.description}
-                            </p>
+                                {/* Университет */}
+                                <p>
+                                    <strong>Университет: </strong>
+                                    <a href={project.link_university} target="_blank" rel="noopener noreferrer">
+                                        {project.university}
+                                    </a>
+                                </p>
 
-                            {/* Университет */}
-                            <p>
-                                <strong>Университет: </strong>
-                                <a href={project.link_university} target="_blank" rel="noopener noreferrer">
-                                    {project.university}
-                                </a>
-                            </p>
+                                {/* Компания */}
+                                <p>
+                                    <strong>Компания: </strong>
+                                    <a href={project.link_company} target="_blank" rel="noopener noreferrer">
+                                        {project.company}
+                                    </a>
+                                </p>
 
-                            {/* Компания */}
-                            <p>
-                                <strong>Компания: </strong>
-                                <a href={project.link_company} target="_blank" rel="noopener noreferrer">
-                                    {project.company}
-                                </a>
-                            </p>
+                                {/* Зарплата */}
+                                <p><strong>Зарплата: </strong> {project.salary} рублей/мес</p>
 
-                            {/* Зарплата */}
-                            <p>
-                                <strong>Зарплата: </strong> {project.salary} рублей/мес
-                            </p>
+                                {/* Время занятости */}
+                                <p><strong>Время занятости: </strong> {project.employmentType}</p>
 
-                            {/* Время занятости */}
-                            <p>
-                                <strong>Время занятости: </strong> {project.employmentTime}
-                            </p>
+                                {/* Требуемое образование */}
+                                <p><strong>Требуемое образование: </strong> {project.education}</p>
 
-                            {/* Требуемое образование */}
-                            <p>
-                                <strong>Требуемое образование: </strong> {project.education}
-                            </p>
+                                {/* Формат стажировки */}
+                                <p><strong>Формат стажировки: </strong> {project.internshipFormat}</p>
 
-                            {/* Формат стажировки */}
-                            <p>
-                                <strong>Формат стажировки: </strong> {project.intershipFormat}
-                            </p>
-
-                            {/* Количество мест */}
-                            <p>
-                                <strong>Количество мест: </strong> {project.places}
-                            </p>
+                                {/* Количество мест */}
+                                <p><strong>Количество мест: </strong> {project.places}</p>
+                            </div>
                         </div>
                     </div>
                 )}
